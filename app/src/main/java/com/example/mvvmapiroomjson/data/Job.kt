cc5fd3bbea2cc9870cd271dd0f691df2ea34115d
divya <gulluchaubey1096@gmail.com>
2022-08-22 17:28:28 +0530

    adapter

--- app/src/main/java/com/example/mvvmapiroomjson/data/Job.kt
+++ app/src/main/java/com/example/mvvmapiroomjson/data/Job.kt
@@ -1,7 +1,3 @@
 package com.example.mvvmapiroomjson.data
 
-data class Job(
-    val exp: Int,
-    val organization: String,
-    val role: String
-)
+data class Job(val exp: Int, val organization: String, val role: String)
--- app/src/main/java/com/example/mvvmapiroomjson/ui/AdapterMain.kt
+++ app/src/main/java/com/example/mvvmapiroomjson/ui/AdapterMain.kt
@@ -1,25 +1,31 @@
 package com.example.mvvmapiroomjson.ui
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmapiroomjson.R
+import com.example.mvvmapiroomjson.data.Data
+import com.example.mvvmapiroomjson.databinding.ListItemBinding
 
-class AdapterMain():RecyclerView.Adapter<AdapterMain.ViewHolder>() {
-    inner class ViewHolder(view:View) : RecyclerView.ViewHolder(view){
-        view.bind
+class AdapterMain(private var mcontext:Context,private var data:ArrayList<Data>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    inner class ViewHolder(view:ListItemBinding) : RecyclerView.ViewHolder(view.root){
+        fun setData(data:Data){
+
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        fun bind(holder){}
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context),R.layout.list_item,parent,false))
     }
 
     override fun getItemCount(): Int {
+       return data.size
     }
 
-
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).setData(data[position])
+    }
 }
